6168f15932a9059b49060945d15e9acfee1b8cac
alex <arodriguez202020@gmail.com>
2021-11-01 19:36:33 -0500

    create and render video and clients components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,9 @@
     <HeroSection />
     <main id="main">
       <About />
+      <Counts />
+      <Video />
+      <Clients />
     </main>
   </div>
 </template>
@@ -10,12 +13,18 @@
 <script>
 import HeroSection from "../components/HeroSection.vue";
 import About from "../components/About.vue";
+import Counts from "../components/Counts.vue";
+import Video from "../components/Video.vue";
+import Clients from "../components/Clients.vue";
 
 export default {
   name: "app",
   components: {
     HeroSection,
     About,
+    Counts,
+    Video,
+    Clients,
   },
 };
 </script>
